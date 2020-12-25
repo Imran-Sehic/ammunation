84b0e2470c290b3c52da8a8a48630b01a5dda0a1
Imran <imran.sehic55@gmail.com>
2020-12-25 15:58:57 +0100

    added functionality to update and delete weapons and ammos

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,7 +6,7 @@ const User = require("../models/user")(sequelize, DataTypes);
 
 exports.getProfile = async (req, res, next) => {};
 
-exports.createWeapon = async (req, res, next) => {
+exports.createWeapon = (req, res, next) => {
   const { name, description, weaponType, caliber, price, imageUrl } = req.body;
 
   try {
@@ -29,9 +29,73 @@ exports.createWeapon = async (req, res, next) => {
   }
 };
 
-exports.updateWeapon = (req, res, next) => {};
+exports.updateWeapon = async (req, res, next) => {
+  const weaponId = req.params.weaponId;
+  const { name, description, weaponType, caliber, price, imageUrl } = req.body;
 
-exports.deleteWeapon = (req, res, next) => {};
+  try {
+    const weapon = await Weapon.findByPk(weaponId);
+
+    if (!weapon) {
+      const error = new Error("No weapon with given id was found!");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    if (weapon.userId !== req.userId) {
+      const error = new Error("You are not allowed to edit this weapon!");
+      error.statusCode = 403;
+      throw error;
+    }
+
+    weapon.name = name;
+    weapon.description = description;
+    weapon.weaponType = weaponType;
+    weapon.caliber = caliber;
+    weapon.price = price;
+    weapon.imageUrl = imageUrl;
+
+    await weapon.save();
+
+    res.status(200).json({message: "Weapon successfully updated!"});
+
+  } catch (err) {
+    if(!err.statusCode) {
+      err.statusCode = 500;
+      next(err);
+    }
+  }
+};
+
+exports.deleteWeapon = async (req, res, next) => {
+  const weaponId = req.params.weaponId;
+
+  try {
+    const weapon = await Weapon.findByPk(weaponId);
+
+    if (!weapon) {
+      const error = new Error("No weapon with given id was found!");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    if (weapon.userId !== req.userId) {
+      const error = new Error("You are not allowed to delete this weapon!");
+      error.statusCode = 403;
+      throw error;
+    }
+
+    await weapon.destroy();
+
+    res.status(200).json({message: "Weapon successfully deleted!"});
+
+  } catch (err) {
+    if(!err.statusCode) {
+      err.statusCode = 500;
+      next(err);
+    }
+  }
+};
 
 exports.createAmmo = (req, res, next) => {
   const { name, ammoType, quantity, caliber, price, imageUrl } = req.body;
@@ -56,8 +120,72 @@ exports.createAmmo = (req, res, next) => {
   }
 };
 
-exports.updateAmmo = (req, res, next) => {};
+exports.updateAmmo = (req, res, next) => {
+  const ammoId = req.params.ammoId;
+  const { name, ammoType, quantity, caliber, price, imageUrl } = req.body;
 
-exports.deleteAmmo = (req, res, next) => {};
+  try {
+    const ammo = await Ammo.findByPk(ammoId);
+
+    if (!ammo) {
+      const error = new Error("No ammo with given id was found!");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    if (ammo.userId !== req.userId) {
+      const error = new Error("You are not allowed to edit this ammo!");
+      error.statusCode = 403;
+      throw error;
+    }
+
+    ammo.name = name;
+    ammo.ammoType = ammoType;
+    ammo.quantity = quantity;
+    ammo.caliber = caliber;
+    ammo.price = price;
+    ammo.imageUrl = imageUrl;
+
+    await ammo.save();
+
+    res.status(200).json({message: "Ammo successfully updated!"});
+
+  } catch (err) {
+    if(!err.statusCode) {
+      err.statusCode = 500;
+      next(err);
+    }
+  }
+};
+
+exports.deleteAmmo = (req, res, next) => {
+  const ammoId = req.params.ammoId;
+
+  try {
+    const ammo = await Ammo.findByPk(ammoId);
+
+    if (!ammo) {
+      const error = new Error("No ammo with given id was found!");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    if (ammo.userId !== req.userId) {
+      const error = new Error("You are not allowed to delete this ammo!");
+      error.statusCode = 403;
+      throw error;
+    }
+
+    await ammo.destroy();
+
+    res.status(200).json({message: "Ammo successfully deleted!"});
+
+  } catch (err) {
+    if(!err.statusCode) {
+      err.statusCode = 500;
+      next(err);
+    }
+  }
+};
 
 exports.order = (req, res, next) => {};
